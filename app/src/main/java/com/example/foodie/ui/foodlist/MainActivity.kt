1012638d5e39588a78aff92378915b6f1537837a
Gergely Tóth <cactusss09@gmail.com>
2022-04-17 22:40:39 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/foodie/ui/foodlist/MainActivity.kt
+++ app/src/main/java/com/example/foodie/ui/foodlist/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.foodie.ui.foodlist
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.foodie.R
 import com.example.foodie.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Todo action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
