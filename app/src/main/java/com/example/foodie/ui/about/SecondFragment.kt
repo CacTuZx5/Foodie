964ff3301f6a17113cd0b72224d52b97fb407689
Gergely Tóth <cactusss09@gmail.com>
2022-05-03 22:55:40 +0200

    creating the 3 main screens

--- app/src/main/java/com/example/foodie/ui/about/SecondFragment.kt
+++ app/src/main/java/com/example/foodie/ui/about/SecondFragment.kt
@@ -1,20 +1,18 @@
 package com.example.foodie.ui.about
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.foodie.R
-import com.example.foodie.databinding.FragmentSecondBinding
+import androidx.fragment.app.Fragment
+import com.example.foodie.databinding.FragmentAboutBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SecondFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentAboutBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +23,7 @@ class SecondFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -33,9 +31,9 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
+        /*binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+        }*/
     }
 
     override fun onDestroyView() {
